214c75c9882c95c4ff3c502f095966539f64f7de
Risa2506 <risa2506.mile@gmail.com>
2025-07-07 15:41:05 +0530

    amazon-html-css

---
+++ data/products.js
@@ -0,0 +1,21 @@
+const products=[
+  { id:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    image:'products-img/athletic-cotton-socks-6-pairs.jpg',
+    rating:{
+      stars:4.5,
+      count:87
+    },
+    priceCents:1090
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "products-img/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095
+  }
+]
