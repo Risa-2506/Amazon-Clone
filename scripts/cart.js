9997be56771e9034f5805898676f4f3f17a869db
Risa2506 <risa2506.mile@gmail.com>
2025-07-08 19:11:22 +0530

    making update interactive

--- scripts/cart.js
+++ scripts/cart.js
@@ -31,11 +31,13 @@ cart.forEach((item) =>
                 </div>
                 <div class="quantity-update-delete">
                   <span class="quantity">
-                    Quantity: ${item.quantity}
+                    Quantity: <span class="js-quantity-${matchingitem.id}">${item.quantity}</span>
                   </span>
-                  <span class="update-link">
+                  <span class="update-link js-update-link-${matchingitem.id}" data-matchingitem-id="${matchingitem.id}">
                     Update
                   </span>
+                  <input type="number" class="input-number js-input-number-${matchingitem.id}">
+                  <span class="Save js-save-link-${matchingitem.id}" data-matching-item-id="${matchingitem.id}">Save</span>
                   <span class="delete-link">
                     Delete
                   </span>
@@ -89,12 +91,50 @@ cart.forEach((item) =>
           </div>
          </div>`;
 
-        let cartQuantity=0;
-        cart.forEach((item) =>
-        {
-          cartQuantity+=item.quantity;
-        });
-         document.querySelector('.js-cart-summary').innerHTML=cartHTML;
-         document.querySelector('.js-cart-quantity').innerHTML=cartQuantity + ' items';
-
 });
+
+let cartQuantity=0;
+cart.forEach((item) =>
+{
+  cartQuantity+=item.quantity;
+});
+document.querySelector('.js-cart-summary').innerHTML=cartHTML;
+document.querySelector('.js-cart-quantity').innerHTML=cartQuantity + ' items';
+
+document.querySelectorAll('.update-link')
+  .forEach((update)=>
+  {
+    update.addEventListener('click',()=>
+      {
+        const matchingitemId=update.dataset.matchingitemId;
+        const input = document.querySelector(`.js-input-number-${matchingitemId}`);
+        const updateLink=document.querySelector(`.js-update-link-${matchingitemId}`);
+        const saveLink=document.querySelector(`.js-save-link-${matchingitemId}`);
+        const quantity=document.querySelector(`.js-quantity-${matchingitemId}`);
+        input.classList.add('input-display');
+        updateLink.classList.add('update-display');
+        saveLink.classList.add('save-display');
+        quantity.classList.add('quantity-display');
+      }
+    );
+  });
+
+document.querySelectorAll('.Save')
+  .forEach((save)=>
+  {
+    save.addEventListener('click',()=>
+    {
+      const matchingitemId=save.dataset.matchingItemId;
+      const input = document.querySelector(`.js-input-number-${matchingitemId}`);
+      const updateLink=document.querySelector(`.js-update-link-${matchingitemId}`);
+      const saveLink=document.querySelector(`.js-save-link-${matchingitemId}`);
+      const quantity=document.querySelector(`.js-quantity-${matchingitemId}`);
+      const newQuantity=Number(input.value);
+      document.querySelector(`.js-quantity-${matchingitemId}`).innerHTML=newQuantity;
+      input.classList.remove('input-display');
+      updateLink.classList.remove('update-display');
+      saveLink.classList.remove('save-display');
+      quantity.classList.remove('quantity-display');
+    });
+    
+  });
